02e094a39f6e31c9ecafd012d9ac3d7ceeb1a87c
Dennny <keplerde@gmail.com>
2022-12-07 16:59:04 +0300

    ChangeUserAgent

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -1,11 +1,32 @@
 from selenium import webdriver
 import time
-url = "https://www.instagram.com/"
-driver = webdriver.Chrome(executable_path="/chromedriver/chromedriver_win32/chromedriver.exe")
+import random
+from fake_useragent import UserAgent
+# url = "https://www.instagram.com/"
+
+user_agents_list = [
+    "hello_world",
+    "best_of_the_best",
+    "python_today"
+]
+
+useragent = UserAgent()              # создадим обьект класса
+# чтобы передавать опции в наш браузер, нам нужно создать обьект опций
+options = webdriver.ChromeOptions()
+# options.add_argument("user-agent=HelloWorld)")              # подмена UserAgent
+# options.add_argument("user-agent=Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36")
+# options.add_argument(f"user-agent={random.choice(user_agents_list)}")
+options.add_argument(f"user-agent={useragent.random}")       # далее мы вызываем и получае рандомный клиентский адрес
+
+
+driver = webdriver.Chrome(
+    executable_path="/chromedriver/chromedriver_win32/chromedriver.exe",
+    options=options
+)
 
 
 try:
-    driver.get(url=url)                   # перейти о заданной выше ссылке
+    driver.get(url="https://www.whatismybrowser.com/detect/what-is-my-user-agent/")                   # перейти по заданной ссылке
     time.sleep(5)
     # driver.get(url="https://vk.com/")
     # time.sleep(5)
